31993f5f36ab7fe50daaecebf4dd5e7cc34b0a95
AlexT <alex.thummas66@gmail.com>
2024-05-14 21:07:21 -0400

    fix security vulnerability

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./index.css";
-import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,7 +31,6 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         {children}
-        <Analytics />
       </body>
     </html>
   );
